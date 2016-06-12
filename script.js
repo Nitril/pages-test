c7cfe55bbefdc07302b5e9e81f04c05662b9f21a
Nitril <athlonxp@poczta.fm>
2016-06-12 14:40:19 +0200

    webtst

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-function sprawdz() {
-	
-	var liczba = 7;
-	if (liczba>0) document.getElementById("streamtest").innerHTML="<p>FASC 4</p>";
-	else if (liczba < 0 ) document.getElementById("streamtest").innerHTML="ujemna";
-	else if (liczba == 0) document.getElementById("streamtest").innerHTML="zero";
-	else document.getElementById("streamtest").innerHTML="To nie jest liczba cwaniaczku";
-	
-  // handle message
-};
+
+var xmlhttp = new XMLHttpRequest();
+var url = "http://finance.google.com/finance/info?client=ig&q=INDEXNASDAQ:NDX";
+
+xmlhttp.onreadystatechange=function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        myFunction(xmlhttp.responseText);
+    }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
 
     // document.getElementById("streamtest").innerHTML = readBody(xhr);
 
+// function sprawdz() {
+	
+	// var liczba = 7;
+	// if (liczba>0) document.getElementById("streamtest").innerHTML="<p>FASC 4</p>";
+	// else if (liczba < 0 ) document.getElementById("streamtest").innerHTML="ujemna";
+	// else if (liczba == 0) document.getElementById("streamtest").innerHTML="zero";
+	// else document.getElementById("streamtest").innerHTML="To nie jest liczba cwaniaczku";
+	
+  handle message
+// };
 
+function myFunction(response) {
+    var arr = JSON.parse(response);
+    var i;
+    var out = "<table>";
 
+    for(i = 0; i < arr.length; i++) {
+        out += "<tr><td>" +
+        arr[i].id +
+         "</td><td>" //+
+        // arr[i].City +
+        // "</td><td>" +
+        // arr[i].Country +
+        // "</td></tr>"
+		;
+    }
+    out += "</table>";
+    document.getElementById("streamtest").innerHTML = out;
+}
