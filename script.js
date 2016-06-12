150542104c9a34e056a8c4a6e3ff80c609ef5dc2
Nitril <athlonxp@poczta.fm>
2016-06-12 13:18:51 +0200

    xhr

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    function readBody(xhr) {
+    var data;
+    if (!xhr.responseType || xhr.responseType === "text") {
+        data = xhr.responseText;
+    } else if (xhr.responseType === "document") {
+        data = xhr.responseXML;
+    } else {
+        data = xhr.response;
+    }
+    return data;
+}
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+        console.log(readBody(xhr));
+    }
+}
+xhr.open('GET', 'https://github.com/', true);
+xhr.send(null);
+    // document.getElementById("streamtest").innerHTML = readBody(xhr);
+
+
+
