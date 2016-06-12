b61b63a9548587858b1d6f990eff079e1e6d513e
Nitril <athlonxp@poczta.fm>
2016-06-12 14:53:02 +0200

    web 44

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ xmlhttp.send();
 function myFunction(response) {
     var arr = JSON.parse(response);
     var i;
-    var out //= "<table>";
+    var out; //= "<table>";
 
     //for(i = 0; i < arr.length; i++) {
         out = arr.id; //+
