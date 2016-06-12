62323215155b537ef9a0ce9ebc6ed90243b9e210
Nitril <athlonxp@poczta.fm>
2016-06-12 14:06:47 +0200

    streamtest id

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function sprawdz() {
 	
 	var liczba = 7;
-	if (liczba>0) document.getElementById("wynik").innerHTML="dodatnia"; -->
-	else if (liczba < 0 ) document.getElementById("wynik").innerHTML="ujemna";
-	else if (liczba == 0) document.getElementById("wynik").innerHTML="zero";
-	else document.getElementById("wynik").innerHTML="To nie jest liczba cwaniaczku";
+	if (liczba>0) document.getElementById("streamtest").innerHTML="dodatnia"; -->
+	else if (liczba < 0 ) document.getElementById("streamtest").innerHTML="ujemna";
+	else if (liczba == 0) document.getElementById("streamtest").innerHTML="zero";
+	else document.getElementById("streamtest").innerHTML="To nie jest liczba cwaniaczku";
 	
   // handle message
 };
