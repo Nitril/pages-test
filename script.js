3bb79ec846e0258383b35eaf58240b2abd829f8e
Nitril <athlonxp@poczta.fm>
2016-06-12 14:58:13 +0200

    war 5

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ xmlhttp.onreadystatechange=function() {
         myFunction(xmlhttp.responseText);
     }
 }
-xmlhttp.open("GET", url, true);
+xmlhttp.open("GET", url, false);
 xmlhttp.send();
 
     // document.getElementById("streamtest").innerHTML = readBody(xhr);
@@ -25,7 +25,6 @@ xmlhttp.send();
 
 function myFunction(response) {
     var arr = JSON.parse(response);
-    var i;
     var out; //= "<table>";
 
     //for(i = 0; i < arr.length; i++) {
@@ -37,7 +36,7 @@ function myFunction(response) {
 		//;
     //}
     //out += "</table>";
-    document.getElementById("streamtest").innerHTML = <p>out</p>;
+    document.getElementById("streamtest").innerHTML = "<p>"+out+"</p>";
 };
 
 }
