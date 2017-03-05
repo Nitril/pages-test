78c9aa0cc4411d49ce5c8cfeae225a6edff9eff1
Nitril <athlonxp@poczta.fm>
2017-03-05 23:44:31 +0100

    dasd
    sdas

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ var character_name = "Atrophie"
 const apikey = "7djtrck735sj9vj9kmtdkyxdaqvqm9f9";
 var chlink =  "https://eu.api.battle.net/wow/auction/data/"+realm+ "?locale=en_GB&apikey="+apikey;
 document.getElementById("myBtn").onclick = function() {
+    $('#myBtn').click(sprawdz());
 mydiv = document.getElementById("myBtn");
  if (mydiv) { 
     
@@ -36,6 +37,6 @@ console.log(xhr.response);
 alert('clicked');
 
 };
-$('#myBtn').click(sprawdz());
+
 
 }
