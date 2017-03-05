fdf5c88a7db201146a4dc75cfa059fc06c08d511
Nitril <athlonxp@poczta.fm>
2017-03-06 00:22:46 +0100

    ioui

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ var character_name = "Atrophie"
 const apikey = "7djtrck735sj9vj9kmtdkyxdaqvqm9f9";
 var chlink =  "https://eu.api.battle.net/wow/auction/data/"+realm+ "?locale=en_GB&apikey="+apikey;
 
-  $('#myBtn').click(sprawdz());
+  
 var sprawdz = function () {
 
 var xhr = new XMLHttpRequest();
@@ -32,7 +32,7 @@ console.log(xhr.response);
 alert('clicked');
 
 };
-
+$('#myBtn').click(sprawdz);
 // if (mydiv) { 
     
 //  mydiv.addEventListener("click", sprawdz, false);
