00071eb7143ce6746434d55534a2becb5c872df8
Nitril <athlonxp@poczta.fm>
2017-03-06 01:19:02 +0100

    r6rftg

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ var chlink =  "https://eu.api.battle.net/wow/auction/data/"+realm+ "?locale=en_G
 
   
 
-$('#myBtn').on('click', function() {
+$('#myBtn').on('click', function sprawdz() {
 var xhr = new XMLHttpRequest();
 
 xhr.open("GET",chlink , false);
